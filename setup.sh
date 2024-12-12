7338adfe0bc2405bc31a6e5ccfcf90bdd20b1ee3
Arturo Laurenzi <arturo.laurenzi@gmail.com>
2024-12-12 11:41:04 +0100

    Update setup.sh for ros2

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ export PATH=$PATH:/opt/xbot/bin
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/opt/xbot/lib
 export PYTHONPATH=$PYTHONPATH:/opt/xbot/lib/python2.7/dist-packages:/opt/xbot/lib/python3/dist-packages
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/opt/xbot/share:/opt/xbot/lib
+export AMENT_PREFIX_PATH=$AMENT_PREFIX_PATH:/opt/xbot
 mkdir -p $HOME/.xbot
 touch $HOME/.xbot/active_config
 export XBOT_CONFIG=$HOME/.xbot/active_config

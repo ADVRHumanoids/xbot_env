0653236c216611d3bd03f84fc2f19350d7d76c67
Arturo Laurenzi <arturo.laurenzi@iit.it>
2024-12-13 16:08:50 +0100

    jammy/humble

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/xbot/lib
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/xbot
 export PATH=$PATH:/opt/xbot/bin
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/opt/xbot/lib
-export PYTHONPATH=$PYTHONPATH:/opt/xbot/lib/python2.7/dist-packages:/opt/xbot/lib/python3/dist-packages
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/opt/xbot/share:/opt/xbot/lib
+export PYTHONPATH=$PYTHONPATH:/opt/xbot/lib/python3.10/site-packages
 export AMENT_PREFIX_PATH=$AMENT_PREFIX_PATH:/opt/xbot
 mkdir -p $HOME/.xbot
 touch $HOME/.xbot/active_config
